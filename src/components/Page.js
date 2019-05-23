9f81e028fdafc1ba881f8dd469d0ca667522902f
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-05-23 13:08:26 -0300

    finished

---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Page = () =>(
+    <div>
+        <h1 style={{ color: 'green' }}>
+            Dashboard
+        </h1>
+    </div>
+);
+
+export default Page;
---
+++ src/components/reports.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Reports = () =>(
+    <div>
+        <h1 style={{ color: 'red' }}>
+            Reports
+        </h1>
+    </div>
+)
+
+export default Reports;
