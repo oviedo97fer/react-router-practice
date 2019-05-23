67d8f28da89beb610cb50bd05c9b3f8c371b5547
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-05-23 12:45:40 -0300

    finish

---
+++ src/components/settings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Settings = () =>(
+    <div>
+        <h1 style={{ color: 'blue' }}>
+            Settings
+        </h1>
+    </div>
+)
+
+export default Settings;
